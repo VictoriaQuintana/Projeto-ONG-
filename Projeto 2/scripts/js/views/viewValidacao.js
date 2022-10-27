9aceda290da9faa41b20d0a1c94b8e8a93ad2dfe
VictoriaQuintana <114583009+VictoriaQuintana@users.noreply.github.com>
2022-10-27 11:24:06 -0300

    Delete Projeto 2 directory

--- Projeto 2/scripts/js/views/viewValidacao.js
+++
@@ -1,8 +0,0 @@
-// Mostrar resposta de Validação no Input
-
-class ValidaView {
-    mostrarValid(inputId, validation) {
-        $(inputId).attr("class", `form-control is-${validation}`)
-    }
-
-}
